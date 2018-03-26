c903732503059005a2baef305cc95cbbea77a458
natinusala <natinusala@gmail.com>
2018-03-26 10:49:06 +0200

    Untested scene stack system

--- engine/engine.py
+++ engine/engine.py
@@ -1,34 +1,71 @@
 class Engine:
 
+    TRANSITION_ACTION_PUSH = 0
+    TRANSITION_ACTION_POP = 1
+
     def __init__(self):
-        self.__sceneStack = None
+        self.__sceneStack = []
         self.__pendingScene = None
         self.__transitionScene = None
+        self.__transitionAction = None
 
     def update(self):
-        if self.__transitionScene is None:
-            self.__scene.update()
+        if self.__transitionScene is None and self.__sceneStack.count() > 0:
+            self.__sceneStack[-1].update()
         else:
             self.__transitionScene.update()
 
     def draw(self):
-        self.__scene.draw()
+        if self.__sceneStack.count() > 0:
+            for i in self.__sceneStack:
+                self.__sceneStack[i].draw()
 
         if self.__transitionScene is not None:
             self.__transitionScene.draw()
 
     def onTransitionFinish(self):
-        self.changeScene(self.__pendingScene)
-        self.__pendingScene = None
-        self.__transitionScene = None
+        if self.__transitionAction == Engine.TRANSITION_ACTION_PUSH:
+            # push pending scene and clear transition
+            self.__transitionScene = None
+            self.pushScene(self.__pendingScene, None)
+            self.__pendingScene = None
+        elif self.__transitionAction == Engine.TRANSITION_ACTION_POP:
+            #pop current scene and clear transition
+            self.__transitionScene = None
+            self.popScene(None)
 
-    def transitionToScene(self, scene, transition):
-        self.__pendingScene = scene
-        self.__transitionScene = transition
+    def pushScene(self, scene, transition):
+        # transition
+        if transition is not None:
+            self.__transitionAction = Engine.TRANSITION_ACTION_PUSH
+            self.__transitionScene = transition
+            self.__pendingScene = scene
+            # we now wait for onTransitionFinish()
+        else:
+            # pause the current active scene
+            if self.__sceneStack.count() > 0:
+                self.__sceneStack[-1].onPause()
 
-    def changeScene(self, scene):
-        if self.__scene is not None:
-            self.__scene.unload()
+            # load and push the new one
+            scene.load()
+            self.__sceneStack.append(scene)
 
-        self.__scene = scene
-        self.__scene.load()
+        pass
+
+    def popScene(self, transition):
+        if transition is not None:
+            self.__transitionAction = Engine.TRANSITION_ACTION_POP
+            self.__transitionScene = transition
+            # we now wait for onTransitionFinish()
+        else:
+            # unload and pop the current scene
+            if self.__sceneStack.count() > 0:
+                self.__sceneStack[-1].unload()
+                self.__sceneStack.pop()
+
+                # if the stack contains at least one scene
+                # after popping, we resume it
+                if self.__sceneStack.count() > 0:
+                    self.__sceneStack[-1].onResume()
+
+        pass
--- engine/scene/scene.py
+++ engine/scene/scene.py
@@ -1,19 +1,25 @@
 class Scene:
 
-	def __init__(self, engine):
-		self.__engine = engine
+    def __init__(self, engine):
+        self.__engine = engine
 
-	def load():
+    def load(self):
+        pass
+
+    def unload(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+    def transitionOut(self):
+        return False
+
+	def onPause(self):
 		pass
 
-	def unload():
+	def onResume(self):
 		pass
-
-	def draw():
-		pass
-
-	def update():
-		pass
-
-	def transitionOut():
-		return False
