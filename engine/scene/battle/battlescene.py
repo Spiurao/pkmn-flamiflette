ac4dbd9da654fdc143f3f36c5757a6f8832ff9c2
spiurao <spiurao@gmail.com>
2018-04-16 14:32:47 +0200

    Added fight scene stub

--- engine/scene/battle/battlescene.py
+++ engine/scene/battle/battlescene.py
@@ -1,6 +1,8 @@
 from engine.scene.scene import Scene
 
 class BattleScene(Scene):
-	def __init__(self, engine, background):
-		self.engine = engine
-		self.background = background
+	def __init__(self, engine, background, player):
+		super().__init__(engine)
+
+		self.__background = background
+		self.__player = player
