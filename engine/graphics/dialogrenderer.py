5eec3742601d6d48adb16b1d24b9ca9ba7c46cc2
natinusala <natinusala@gmail.com>
2018-04-18 10:20:24 +0200

    Added DialogRenderer and FontManager classes prototypes

---
+++ engine/graphics/dialogrenderer.py
@@ -0,0 +1,11 @@
+from typing import Tuple
+
+
+class DialogRenderer:
+
+    def __init__(self, boundaries : Tuple, text : str):
+        self.__boundaries = boundaries
+        self.__text = text
+
+
+
---
+++ engine/graphics/fontmanager.py
@@ -0,0 +1,13 @@
+class FontManager:
+
+    @staticmethod
+    def load():
+        # TODO Load all fonts
+        pass
+
+    @staticmethod
+    def unload():
+        # TODO Unload all fonts
+        pass
+
+    # TODO Fonts getters
