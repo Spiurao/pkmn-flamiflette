ed4a612d15d276ee97a7dabfcdcc036001e433e7
natinusala <natinusala@gmail.com>
2018-04-18 19:48:28 +0200

    Super awesome Dialog Frame Vol. 2 : "it doesn't work yet but I'm hungry so I'm leaving"

--- engine/graphics/dialogframe.py
+++ engine/graphics/dialogframe.py
@@ -1,13 +1,55 @@
+import re
 from typing import Tuple, Callable
 
 import pygame
+import pypeg2
 from pygame.surface import Surface
+from pypeg2 import contiguous, attr, parse, word, maybe_some
 
 from engine.graphics.fontmanager import FontManager
 from engine.graphics.frame import Frame
 from engine.graphics.textures import Textures
 from engine.timer import Timer
 
+class Text(pypeg2.List):
+    pass
+
+class BoldText(pypeg2.List):
+    grammar = "<b>", Text, "</b>"
+
+class UnderlineText(pypeg2.List):
+    grammar = "<u>", Text, "</u>"
+
+class ShakingText(pypeg2.List):
+    grammar = "<shaking>", Text, "</shaking>"
+
+class StrikeText(pypeg2.List):
+    grammar = "<strike>", Text, "</strike>"
+
+class ItalicText(pypeg2.List):
+    grammar = "<i>", Text, "</i>"
+
+class WavingText(pypeg2.List):
+    grammar = "<waving>", Text, "</waving>"
+
+class SmallText(pypeg2.List):
+    grammar = "<small>", Text, "</small>"
+
+class BigText(pypeg2.List):
+    grammar = "<big>", Text, "</big>"
+
+class SlowText(pypeg2.List):
+    grammar = "<slow>", Text, "</slow>"
+
+class FastText(pypeg2.List):
+    grammar = "<fast>", Text, "</fast>"
+
+class ColoredText(pypeg2.List):
+    grammar = "<color ", pypeg2.Symbol, ">", Text, "</color>"
+
+# Ugh not this again
+Text.grammar = attr("text", contiguous(maybe_some([SlowText, FastText, SmallText, BigText, ColoredText, WavingText, StrikeText, ShakingText, ItalicText, BoldText, UnderlineText,
+                                                   re.compile(r"[^<>*]")])))
 
 class DialogFrame:
 
@@ -20,7 +62,7 @@ class DialogFrame:
 
     # TODO Move the caret at the end of the text for each "page"
 
-    FONT = "Emerald26Regular"
+    FONT = "Emerald32Regular"
 
     CARET_TIMER_DURATION = 100
 
@@ -40,6 +82,10 @@ class DialogFrame:
         self.__frame = Frame(boundaries, self.__window)
         self.__surface = FontManager.getFont(DialogFrame.FONT).render(self.__text, True, (0, 0, 0, 0))
         self.__caretPosition = (self.__boundaries[2] + self.__boundaries[0] - Frame.PADDING*2, self.__boundaries[3] + self.__boundaries[1] - Frame.PADDING*2, 20, 20)
+
+        # Rich text parsing
+        self.__tree = parse(self.__text, Text)
+        
 
     def caretTimerCb(self, tag):
         self.__caretStep = (self.__caretStep + 1) % 4
--- engine/graphics/fontmanager.py
+++ engine/graphics/fontmanager.py
@@ -18,7 +18,7 @@ class FontManager:
 
     # File name, size, style
     FONT_FACES = {
-        "Emerald26Regular" : ("Emerald", 26, FontStyle.REGULAR),
+        "Emerald32Regular" : ("Emerald", 32, FontStyle.REGULAR),
         "Emerald32Bold" : ("Emerald", 32, FontStyle.BOLD)
     }
 
